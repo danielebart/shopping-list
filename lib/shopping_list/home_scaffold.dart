22f2a4d0fb0c8f4fb706c7c8f3c7e72b34999c43
Daniele Bartorilla <danielebartorilla@gmail.com>
2019-09-24 22:47:27 +0200

    add new tab page in bottombar and animate fab

--- lib/shopping_list/home_scaffold.dart
+++ lib/shopping_list/home_scaffold.dart
@@ -3,10 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'manage_shopping_list/add_item/add_shopping_item_widget.dart';
+import 'manage_shopping_list/archive/archive_list_widget.dart';
 import 'manage_shopping_list/items/shopping_list_provider.dart';
 import 'manage_shopping_list/items/shopping_list_widget.dart';
 
-class HomeScaffold extends StatelessWidget {
+class HomeScaffold extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScaffoldState();
+  }
+}
+
+class _HomeScaffoldState extends State<HomeScaffold>
+    with SingleTickerProviderStateMixin {
+  var _currentIndex = 0;
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 1000), vsync: this, value: 0);
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,27 +35,49 @@ class HomeScaffold extends StatelessWidget {
         elevation: 0.0,
         backgroundColor: Colors.grey[50],
       ),
-      body: Center(
-        child: ShoppingListWidget(),
-      ),
+      body: _widgetFromSelectedIndex(),
       bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.list), title: Text('lista corrente')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.archive), title: Text('Archivio'))
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (() => showModalBottomSheet(
-                isScrollControlled: true,
-                context: context,
-                builder: (context) => AddItemWidget()).then((value) {
-              var provider = Provider.of<ShoppingListProvider>(context);
-              if (value != null) provider.onItemAdded(value);
-            })),
-        child: Icon(Icons.add_shopping_cart),
+          currentIndex: _currentIndex,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list), title: Text('lista corrente')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.archive), title: Text('Archivio'))
+          ],
+          onTap: (index) {
+            setState(() => _currentIndex = index);
+          }),
+      floatingActionButton: ScaleTransition(
+        scale: _animation,
+        child: FloatingActionButton(
+          onPressed: (() =>
+              showModalBottomSheet(
+                  isScrollControlled: true,
+                  context: context,
+                  builder: (context) => AddItemWidget()).then((value) {
+                var provider = Provider.of<ShoppingListProvider>(context);
+                if (value != null) provider.onItemAdded(value);
+              })),
+          child: Icon(Icons.add_shopping_cart),
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  _widgetFromSelectedIndex() {
+    switch (_currentIndex) {
+      case 0:
+        _controller.forward();
+        return ShoppingListWidget();
+      case 1:
+        _controller.reverse();
+        return ArchiveListWidget();
+    }
   }
 }
---
+++ lib/shopping_list/manage_shopping_list/archive/archive_list_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ArchiveListWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
